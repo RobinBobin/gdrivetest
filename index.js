cefdc717c204a98eeca1f6615c09e874b4a8682a
Ruben Shalimov <r_shalimov@inbox.ru>
2021-06-09 23:40:29 +0300

    1) "@robinbobin/react-native-google-drive-api-wrapper": "^0.3.0". 2) Release built.

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
 
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import {
-  Files,
   GDrive,
-  blobToByteArray
+  MimeTypes
 } from "@robinbobin/react-native-google-drive-api-wrapper";
 import React, {
   useEffect
@@ -31,18 +30,71 @@ function App() {
         await GoogleSignin.hasPlayServices();
         await GoogleSignin.signIn();
         
-        global.gdrive = new GDrive();
+        gdrive = new GDrive();
+        gdrive.accessToken = (await GoogleSignin.getTokens()).accessToken;
         
-        global.gdrive.accessToken = (await GoogleSignin.getTokens()).accessToken;
-        global.gdrive.files = new Files();
+        // gdrive.files.fetchCoercesTypes = true;
+        // gdrive.files.fetchRejectsOnHttpErrors = true;
         
-        // console.log(await (await global.gdrive.files.list()).json());
+        // console.log(await (await gdrive.files.list({
+        //   q: "name='GDocJSON'"
+        // })).json());
         
-        console.log(await (await global.gdrive.files.get("1K8zqOD_KqCzgU0Qerq9NhndSXOD3iyDp", {alt: "media"})).text());
+        // console.log(await gdrive.files.getJson("1fNnvTc2ud_f_eWb_xCgKQg4mgp5YIdE2"));
         
-        console.log(Array.from(await blobToByteArray(await (await global.gdrive.files.get("1qXgpUYTOc4b4wUzMkIk31MeKBH8KFJYF", {alt: "media"})).blob())));
+        // console.log(await gdrive.files.getText("1K8zqOD_KqCzgU0Qerq9NhndSXOD3iyDp"));
+        
+        // console.log(await gdrive.files.getMetadata("1K8zqOD_KqCzgU0Qerq9NhndSXOD3iyDp"));
+        
+        // console.log(await gdrive.files.getBinary("1qXgpUYTOc4b4wUzMkIk31MeKBH8KFJYF", null, "1-1"));
+        
+        // console.log(await gdrive.files.emptyTrash());
+        
+        // console.log(await gdrive.files.copy("1qXgpUYTOc4b4wUzMkIk31MeKBH8KFJYF"));
+        
+        // const id = "1-ROyra6bD37MrzMCqBBFONPrCNOSaO9xXdPVy_RBjos";
+        // const id = "1RHTSHeJoKIWzEPc6M85AkkVowXu7j0oP3e1SRJQPR2Q";
+        
+        // console.log(await gdrive.files.export(id, {mimeType: MimeTypes.CSV}));
+        
+        // console.log(await gdrive.files.generateIds());
+        
+        // console.log(await gdrive.files.delete("1aBo6eg5j3uWS8NLvXUJZ2yiCll6XYInV"));
+        
+        // console.log(await gdrive.files.newMediaUploader()
+        //   .setData("english русский französisch", MimeTypes.TEXT)
+        //   .setIdOfFileToUpdate("1K8zqOD_KqCzgU0Qerq9NhndSXOD3iyDp")
+        //   .execute());
+        
+        // console.log((await gdrive.files.newMultipartUploader()
+        //   .setData("cm9iaW4=", MimeTypes.TEXT)
+        //   .setIsBase64(true)
+        //   .setRequestBody({
+        //     name: "multi_text",
+        //   })
+        //   .execute()).id);
+        
+        // console.log(await gdrive.files.newMultipartUploader()
+        //   .setData([1, 2, 3, 4, 5], MimeTypes.BINARY)
+        //   .setRequestBody({
+        //     name: "multi_bin",
+        //   })
+        //   .execute());
+        
+        // console.log(await gdrive.files.newMultipartUploader()
+        //   // .setIdOfFileToUpdate("10pxt9J5dj4VQ_2YA_2WMueTUGWjnqPwO")
+        //   .setData("Hooray!!!", MimeTypes.TEXT)
+        //   .setIdOfFileToUpdate("1K8zqOD_KqCzgU0Qerq9NhndSXOD3iyDp")
+        //   .setRequestBody({
+        //     name: "text_changed_Hooray!!!"
+        //   })
+        //   .execute());
+        
+        console.log(await gdrive.about.get({fields: "user"}));
+        
+        console.log("ok");
       } catch (error) {
-        console.log("oops", error, error.code);
+        console.log("oops", error.toString());
       }
     })();
   }, []);
